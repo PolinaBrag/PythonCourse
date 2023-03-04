2e5291aef77c5c531a0b5dba4e1e6e56a2a00920
Полина Брагина <polyandriys14@mail.ru>
2023-03-04 14:35:32 +0300

    new commit

--- Seminar_11/Task7.py
+++ Seminar_11/Task7.py
@@ -3,26 +3,36 @@ from random import randint
 
 
 class Matrix:
+    """
+    Создает пустую матрицу по заданному количеству строк и столбцов.
+    """
     def __init__(self, row_count, column_count):
+        """Инициализирует экземпляр класса Matrix."""
         self.row_count = int(row_count)
         self.column_count = int(column_count)
         self.matrix = []
 
     def __new__(cls, *args, **kwargs):
+        """Возвращает экземпляр класса Matrix."""
         instance = super().__new__(cls)
         return instance
 
     def random_fill_matrix(self):
+        """Заполняет матрицу рандомными числами."""
+        MIN_NUM = 1
+        MAX_NUM = 9
         for row in range(self.row_count):
             chank = []
             for col in range(self.column_count):
-                chank.append(randint(1, 9))
+                chank.append(randint(MIN_NUM, MAX_NUM))
             self.matrix.append(chank)
 
     def __str__(self):
+        """Выводит матрицу в консоль."""
         return '\n'.join('  '.join(map(str, row)) for row in self.matrix)
 
     def __add__(self, other):
+        """Возвращает новую матрицу, являющуюся суммой двух других матриц."""
         if self.row_count != other.row_count or self.column_count != other.column_count:
             return 'Нельзя сложить матрицы разного размера'
         else:
@@ -35,6 +45,7 @@ class Matrix:
         return new_matrix
 
     def __eq__(self, other):
+        """Возвращает результат сравнения двух матриц."""
         if self.row_count != other.row_count or self.column_count != other.column_count:
             return False
         else:
